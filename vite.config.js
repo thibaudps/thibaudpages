89ca01ba5a40edcfec7aa7d6b87837805a79c6d7
Thibaud Pages <thibaudpages@yahoo.fr>
2025-12-19 14:57:20 +0100

    logo

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',  
+  base: '/'  // ← Vérifie que c'est bien '/' et pas '/thibaudpages/'
 })
