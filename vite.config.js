154e7eec5baa445813dba2ee59160afec905c59a
thibaudps <thibaudps@gmail.com>
2026-05-11 20:36:21 +0200

    cleaning

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/'  // ← Vérifie que c'est bien '/' et pas '/thibaudpages/'
-})
